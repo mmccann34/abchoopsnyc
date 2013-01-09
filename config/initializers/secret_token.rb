e1a8a0467516ce5d59848290da88ca401d94ca38
mmccann34 <matthew.m.mccann@gmail.com>
2013-01-09 13:16:20 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abchoops::Application.config.secret_token = '4737123eff0f7c6caa273ffd45b9c953aea8208249597f5cdb29c6001fe98e7269f9cb8f76ce7770714a738942f4329a0fb2d44e6b2492dcbd51200f17c7a5d1'
