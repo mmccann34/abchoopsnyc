3fde51ef0f8d553815d6a5f042ad61f1c216afa0
mmccann34 <mmccann34@server.com>
2013-05-02 23:24:53 -0400

    editing players page

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -5,6 +5,7 @@ $(function() {
     "iDisplayLength": 25,
     "aoColumnDefs": [ { "bSortable": false, "bSearchable": false, "aTargets": [ 0, 1 ] } ],
     "aaSorting": [[3,'asc']],
-    "oLanguage": { "sInfo": "Showing _START_ to _END_ of _TOTAL_ Players" }
+    "oLanguage": { "sInfo": "Showing _START_ to _END_ of _TOTAL_ Players" },
+    "bPaginate": false
   });
 });
