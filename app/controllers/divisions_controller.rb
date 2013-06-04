ce1be7d8fddac133f5384bcc0e709177f52186b1
mmccann34 <mmccann34@server.com>
2013-06-03 22:29:47 -0400

    Added Edit to Division

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -10,16 +10,18 @@ class DivisionsController < ApplicationController
   end
 
   def edit
-    @location = Location.find(params[:id])
+    @season = Season.find(params[:season_id])
+    @division = Division.find(params[:id])
   end
 
-  def update
-    @location = Location.find(params[:id])
-    if @location.update_attributes(params[:location])
-      redirect_to edit_location_url(@location), notice: "Location has been updated."
+  def update 
+    @division = Division.find(params[:id]) 
+    if
+      @division.update_attributes(params[:division]) 
+      redirect_to team_list_edit_season_url(params[:season_id]), notice: "Division has been updated." 
     else
-      render action: "edit", flash: { error: "An error occurred while updating Location." }
-    end
+      render action: "edit", flash: { error: "An error occurred while updating Division." } 
+    end 
   end
 
   def destroy
