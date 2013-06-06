747d8b8fcd897d3f4c3c3b6f0f5ffff2b873260b
Matt McCann <matthew.m.mccann@gmail.com>
2013-06-06 14:52:09 +0000

    auto-populate time dropdowns after selecting the first on New Games page

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,3 +1,13 @@
 $(function() {
   $('[data-behavior~=datepicker]').datepicker({"autoclose": true})
+
+  $('#games_game0_time').change(function() {
+    var count = 1;
+    var selected_index = this.selectedIndex;
+    while ($('#games_game'+count+'_time').length > 0) {
+      selected_index += 4;
+      $('#games_game'+count+'_time')[0].selectedIndex = selected_index;
+      count++;
+    }
+  });
 });
