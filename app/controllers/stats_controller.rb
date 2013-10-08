9280adf885dfafee0fa25d943a207f33fa107ca1
Matt McCann <matthew.m.mccann@gmail.com>
2013-10-08 15:56:35 +0000

    ABC +

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -29,27 +29,25 @@ class StatsController < ApplicationController
     @cumulative_player_stats = @team.cumulative_player_stats(@current_season)
   end
   
-  def show_player
-    if params[:season]
-      @current_season = Season.find(params[:season])
-    else
-      @current_season = Season.current
-    end
-    
+  def show_player 
     @player = Player.find_by_id(params[:id])
-    @game_log = @player.game_log(@current_season)
-    @per_game_stats = @player.per_game_stats(@current_season)
+    
+    @log_season = params[:log] ? Season.find(params[:log]) : Season.current
+    @game_log = @player.game_log(@log_season)
+    @per_game_stats = @player.per_game_stats(@log_season)
+
     @career_season_averages = @player.career_season_averages
     @career_averages = @player.career_averages
     @career_season_totals = @player.career_season_totals
     @average_per_season_totals = @player.average_per_season_totals
-    @current_season_averages = @player.current_season_averages
-    
+    @current_season_averages = @player.season_averages(Season.current)
+
+    @splits_season = params[:splits] ? Season.find(params[:splits]) : Season.current    
     @splits = Hash.new
-    @splits['By Result'] = @player.splits_by_result
-    @splits['By Month'] = @player.splits_by_month
-    @splits['By Time'] = @player.splits_by_time
-    @splits['By Opponent'] = @player.splits_by_opponent
+    @splits['By Result'] = @player.splits_by_result(@splits_season)
+    @splits['By Month'] = @player.splits_by_month(@splits_season)
+    @splits['By Time'] = @player.splits_by_time(@splits_season)
+    @splits['By Opponent'] = @player.splits_by_opponent(@splits_season)
   end
   
   private
