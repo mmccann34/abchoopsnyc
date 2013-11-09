107d915c45ecd5886f0eaac9154b4094ede16031
Matt McCann <matthew.m.mccann@gmail.com>
2013-11-09 15:59:10 +0000

    only show schedules for seasons/leagues with games

--- app/models/league.rb
+++ app/models/league.rb
@@ -6,4 +6,8 @@ class League < ActiveRecord::Base
   def teams(season_id = nil)
     self.team_spots.where(season_id: season_id ? season_id : Season.current).map(&:team)
   end
+  
+  def games(season_id = nil)
+    Game.where(season_id: (season_id ? season_id : Season.current), league_id: self.id)
+  end
 end
