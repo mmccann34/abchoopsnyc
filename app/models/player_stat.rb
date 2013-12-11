0bf6bf4915a937e67878305f736d8ef2b3fd5bb0
Matt McCann <matthew.m.mccann@gmail.com>
2013-12-11 05:00:02 +0000

    minor

--- app/models/player_stat.rb
+++ app/models/player_stat.rb
@@ -7,26 +7,28 @@ class PlayerStat < ActiveRecord::Base
   after_initialize :set_defaults
   
   def set_defaults
-    self.points ||= 0
-    self.fgm ||= 0
-    self.fga ||= 0
-    self.fgpct ||= 0
-    self.twom ||= 0
-    self.twoa ||= 0
-    self.twopct ||= 0
-    self.threem ||= 0
-    self.threea ||= 0
-    self.threepct ||= 0
-    self.ftm ||= 0
-    self.fta ||= 0
-    self.ftpct ||= 0
-    self.orb ||= 0
-    self.drb ||= 0
-    self.trb ||= 0
-    self.ast ||= 0
-    self.stl ||= 0
-    self.blk ||= 0
-    self.fl ||= 0
-    self.to ||= 0
+    if self.has_attribute?(:points)
+      self.points ||= 0
+      self.fgm ||= 0
+      self.fga ||= 0
+      self.fgpct ||= 0
+      self.twom ||= 0
+      self.twoa ||= 0
+      self.twopct ||= 0
+      self.threem ||= 0
+      self.threea ||= 0
+      self.threepct ||= 0
+      self.ftm ||= 0
+      self.fta ||= 0
+      self.ftpct ||= 0
+      self.orb ||= 0
+      self.drb ||= 0
+      self.trb ||= 0
+      self.ast ||= 0
+      self.stl ||= 0
+      self.blk ||= 0
+      self.fl ||= 0
+      self.to ||= 0
+    end
   end
 end
