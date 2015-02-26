630b7dd27fee103710afdf10df53045346a037d1
Emily Simonton <emilysimonton@gmail.com>
2015-02-26 14:33:30 -0500

    top performers ready for scores and schedule page

--- app/models/player.rb
+++ app/models/player.rb
@@ -31,6 +31,18 @@ class Player < ActiveRecord::Base
     "#{name} (#{last_team.try(:name)})"
   end
 
+  def first_name_last_int
+    first_name_last_int = ""
+    if self.first_name && self.first_name != '' && self.last_name && self.last_name != ''
+      first_name_last_int = "#{self.first_name} #{self.last_name[0]}."
+    elsif self.first_name && (!self.last_name || self.last_name == '')
+      first_name_last_int = "#{self.first_name}"
+    elsif (!self.first_name || self.first_name == '') && self.last_name
+      first_name_last_int = "#{self.last_name}"
+    end
+    first_name_last_int
+  end
+
   def height
     "#{height_feet}-#{height_inches}"
   end
