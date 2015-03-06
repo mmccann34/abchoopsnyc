43a5a107e3ac5e1c355f0303722300a8f8b968aa
Matt McCann <matthew.m.mccann@gmail.com>
2015-03-06 01:10:21 +0000

    created top performer

---
+++ db/migrate/20150306004541_create_top_performers.rb
@@ -0,0 +1,16 @@
+class CreateTopPerformers < ActiveRecord::Migration
+  def change
+    create_table :top_performers do |t|
+      t.integer :game_id
+      t.integer :player_id
+      t.string :name
+      t.string :team
+      t.string :stat
+      t.integer :type
+
+      t.timestamps
+    end
+    
+    add_index :top_performers, :game_id
+  end
+end
