7a9ae1307bc81132d86c3ee513dd8a8129df4be3
Matt McCann <matthew.m.mccann@gmail.com>
2019-01-28 21:51:25 -0500

    fix

--- db/migrate/20190128194900_separate_google_calendar_ids.rb
+++ db/migrate/20190128194900_separate_google_calendar_ids.rb
@@ -1,6 +1,6 @@
 class SeparateGoogleCalendarIds < ActiveRecord::Migration
   def change
-    remove_column :games, :google_calendar_id, 
+    remove_column :games, :google_calendar_id
     add_column :games, :home_team_google_calendar_id, :string
     add_column :games, :away_team_google_calendar_id, :string
   end
